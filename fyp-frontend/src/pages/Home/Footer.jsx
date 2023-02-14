2b9a62300a5d75b5903a534fcb67bfe1edc9b990
ayyaz10 <codementee@outlook.com>
2023-02-14 05:16:24 +0500

    update

--- fyp-frontend/src/pages/Home/Footer.jsx
+++ fyp-frontend/src/pages/Home/Footer.jsx
@@ -11,45 +11,45 @@ const Footer = () => {
                         <a href="#"><img src="src/assets/white-logo.png" width="140" alt="Footer Logo" /></a>
                     </div>
                     <p className="my-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt cilisis.</p>
-                    <div className="flex gap-4">
-                        <a href="#"><img src="src/assets/payment/payment-1.webp" alt="master card" /></a>
-                        <a href="#"><img src="src/assets/payment/payment-2.webp" alt="master card" /></a>
-                        <a href="#"><img src="src/assets/payment/payment-3.webp" alt="master card" /></a>
-                        <a href="#"><img src="src/assets/payment/payment-4.webp" alt="master card" /></a>
-                        <a href="#"><img src="src/assets/payment/payment-5.webp" alt="master card" /></a>
+                    <div className="flex gap-3">
+                        <img src="src/assets/payment/payment-1.webp" width="40" alt="master card" />
+                        <img src="src/assets/payment/payment-2.webp" width="40" alt="master card" />
+                        <img src="src/assets/payment/payment-3.webp" width="40" alt="master card" />
+                        <img src="src/assets/payment/payment-4.webp" width="40" alt="master card" />
+                        <img src="src/assets/payment/payment-5.webp" width="40" alt="master card" />
                     </div>
                 </div>
             </div>
             <div className="col-2 flex-1 flex flex-col items-center">
                 <div>
-                    <h3 className="text-xl mb-4">Quick Links</h3>
+                    <h3 className="text-xl font-bold mb-4">Quick Links</h3>
                     <div className="flex flex-col">
-                        <a href="">Collections</a>
-                        <a href="">About</a>
-                        <a href="">Contact</a>
-                        <a href="">FAQ</a>
+                        <a href=""  className="mb-2 hover:opacity-70">Collections</a>
+                        <a href=""  className="mb-2 hover:opacity-70">About</a>
+                        <a href=""  className="mb-2 hover:opacity-70">Contact</a>
+                        <a href=""  className="mb-2 hover:opacity-70">FAQ</a>
                     </div>
                 </div>
             </div>
             <div className="col-2 flex-1 flex flex-col items-center">
                 <div>
-                    <h3 className="text-xl mb-4">Acounts</h3>
+                    <h3 className="text-xl font-bold mb-4">Acounts</h3>
                     <div className="flex flex-col">
-                        <a href="">Collections</a>
-                        <a href="">About</a>
-                        <a href="">Contact</a>
-                        <a href="">FAQ</a>
+                        <a href="" className="mb-2 hover:opacity-70">My Account</a>
+                        <a href="" className="mb-2 hover:opacity-70">Orders Tracking</a>
+                        <a href="" className="mb-2 hover:opacity-70">Checkout</a>
+                        <a href="" className="mb-2 hover:opacity-70">Wishlist</a>
                     </div>
                 </div>
             </div>
             <div className="col-4 flex-1 flex flex-col items-center">
                 <div>
-                    <h3 className="text-xl mb-4">Newsletter</h3>
+                    <h3 className="text-xl font-bold mb-4">Newsletter</h3>
                     <ul className="flex gap-2">
-                        <li><a href="" className="block bg-gray-300 p-2 rounded-full cursor-pointer"><AiFillFacebook /></a></li>
-                        <li><a href="" className="block bg-gray-300 p-2 rounded-full cursor-pointer"><AiFillInstagram /></a></li>
-                        <li><a href="" className="block bg-gray-300 p-2 rounded-full cursor-pointer"><AiFillTwitterSquare /></a></li>
-                        <li><a href="" className="block bg-gray-300 p-2 rounded-full cursor-pointer"><AiFillLinkedin /></a></li>
+                        <li><a href="" className="block text-xl bg-white bg-opacity-30 hover:bg-opacity-50 text p-3 rounded-full cursor-pointer"><AiFillFacebook /></a></li>
+                        <li><a href="" className="block text-xl bg-white bg-opacity-30 hover:bg-opacity-50 text p-3  rounded-full cursor-pointer"><AiFillInstagram /></a></li>
+                        <li><a href="" className="block text-xl bg-white bg-opacity-30 hover:bg-opacity-50 text p-3  rounded-full cursor-pointer"><AiFillTwitterSquare /></a></li>
+                        <li><a href="" className="block text-xl bg-white bg-opacity-30 hover:bg-opacity-50 text p-3  rounded-full cursor-pointer"><AiFillLinkedin /></a></li>
                     </ul>
                 </div>
             </div>
